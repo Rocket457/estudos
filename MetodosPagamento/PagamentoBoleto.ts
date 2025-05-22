e6bc4ab056e0bb0dc7d2aceddb0349a800f9b14c
Rocket457 <71684147+Rocket457@users.noreply.github.com>
2025-05-21 23:30:31 -0300

    codigo duplicado

--- MetodosPagamento/PagamentoBoleto.ts
+++
@@ -1,8 +0,0 @@
-import { MetodoPagamento } from "../../MetodoPagamento";
-
-export class PagamentoBoleto implements MetodoPagamento {
-    processarPagamento(valor: number): void {
-        console.log(`Processando pagamento de R$ ${valor} via Boleto.`);
-        // Lógica para processar o pagamento via Boleto
-    }
-}   
--- MetodosPagamento/PagamentoCartao.ts
+++
@@ -1,8 +0,0 @@
-import { MetodoPagamento } from "../../MetodoPagamento";
-
-export class PagamentoCartao implements MetodoPagamento {
-    processarPagamento(valor: number): void {
-        console.log(`Processando pagamento de R$ ${valor} via Cartao.`);
-        // Lógica para processar o pagamento via Cartao
-    }
-}   
